0c906903a4a5dd22a0b4cc1c22524ad5406512d5
fromruswithlove <work.acc_massayev99@mail.ru>
2026-04-20 09:02:48 +0300

    Add files via upload
    Программа-чистильщик временных файлов. Актуально для тех, кто сталкивается с проблемой, когда кол-во временных файлов становится очень много, а найти путь к ним зачастую проблематично. файл удаляет все последние временные файлы, которые старше определенного задаваемого в коде диапазона

---
+++ Linux-версия.cs
@@ -0,0 +1,41 @@
+﻿//Part__2
+
+using System;
+using System.IO;
+using System.Net;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+class Programma
+{
+    class DeleteInTempFile //~TempFile: IDisposable
+    {
+        static void AppInLinux(string[] args)
+        {
+            //Дата, время
+            var _now = DateTime.Now;
+
+            //paths to ~TempFile
+            var temp_Folder = Path.GetTempPath();   //Тут путь к временным файлам, соответствующий правилам написания и расположения в Linux os
+
+            //Удаляем всё, что старше 48 часов
+            foreach (var FilePath in Directory.GetFiles(temp_Folder))
+            {
+                var __FI__ = new FileInfo(FilePath);
+
+                if (__FI__.CreationTime < _now.AddDays(-2))
+                {
+                    try
+                    {
+                        __FI__.Delete();
+                        Console.WriteLine($"{FilePath} был удалён");
+                    }
+                    catch (Exception ex)
+                    {
+                        //pass
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Версия_рограммы_для_Windows_7+_ver.cs
@@ -0,0 +1,33 @@
+﻿//программа, стирающие все временные фацлы. старшие определнного количетсва дней/недель/лет и т.п.
+
+using System;
+//without System.Management -Direct;
+//with System.Net and with System.Windows instead of the System.Diagnostic and System.Collections.Generic 
+using System.IO;
+using System.Net;
+using System.Windows;
+
+static void _Do(string[] EventArgs)
+{
+
+    int DelDays = 1;    //установить диапазон дней можно в этой переменной
+
+    string[] Files = Directory.GetFiles(path: @"C:\Windows\Temp"); //path к System.Windows;
+
+    foreach (string file in Files)
+    {
+        FileInfo __FI__ = new FileInfo(file);
+
+        if (__FI__.CreationTime < DateTime.Now.AddDays(-DelDays));
+        {
+            try
+            {
+                __FI__.Delete();
+            }
+            catch (Exception Exe) 
+            {
+                //pass
+            }
+        }
+    }
+}
